abdfb78e9d3f1850065cd8581421e10793987611
Bill Tihen <william.tihen@garaio-rem.ch>
2023-02-09 21:24:59 +0100

    file system explorer - central class

---
+++ lib/file_system_explorer.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative './input_parser'
+require_relative './directory_calculator'
+
+# Central class that ties together the other classes and returns results
+class FileSystemExplorer
+  attr_reader :input_parser, :directory_calculator, :file_structure, :directory_sizes
+  private :input_parser, :directory_calculator
+
+  def initialize(input_data_file, input_parser: InputParser, directory_calculator: DirectoryCalculator)
+    @file_structure = { '/' => {} }
+    @input_parser = input_parser.new(input_data_file)
+    @directory_calculator = directory_calculator
+  end
+
+  def run
+    parse_input
+  end
+
+  def parse_input
+    input_parser.run
+    @file_structure = input_parser.file_structure
+  end
+
+  # def calculate_directories
+  #   @directory_sizes = directory_calculator.new(file_structure)
+  #                                          .run.directory_sizes
+  # end
+end
---
+++ spec/file_system_explorer_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative '../lib/file_system_explorer'
+
+describe FileSystemExplorer do # rubocop:disable Metrics/BlockLength
+  subject { described_class.new(test_file) }
+
+  let(:test_file) { 'data/test_input.txt' }
+
+  describe '.new' do
+    it { expect(subject.file_structure).to eq({ '/' => {} }) }
+  end
+
+  # rubocop:disable Style/NumericLiterals
+  describe '#run' do
+    context 'when test_input file' do
+      let(:test_file) { 'data/test_all_commands.txt' }
+      let(:expected_structure) do
+        { '/' =>
+          { 'a' => {
+              'e' => { 'i' => 584 },
+              'f' => 29116,
+              'g' => 2557,
+              'h.lst' => 62596
+            },
+            'b.txt' => 14848514,
+            'c.dat' => 8504156,
+            'd' => {
+              'j' => 4_060_174,
+              'd.log' => 8033020,
+              'd.ext' => 5626152,
+              'k' => 7214296
+            } } }
+      end
+
+      before { subject.run }
+
+      it { expect(subject.file_structure).to eq(expected_structure) }
+    end
+  end
+  # rubocop:enable Style/NumericLiterals
+end
