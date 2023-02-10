bd5537b49e3dd9d6f9573be27e578248f6a66ace
Bill Tihen <william.tihen@garaio-rem.ch>
2023-02-10 08:48:37 +0100

    Determine all file path combinations

--- lib/calculators/directory.rb
+++ lib/calculators/directory.rb
@@ -12,6 +12,29 @@ module Calculators
     end
 
     def run
+      @paths = all_unique_paths(file_structure)
+    end
+
+    private
+
+    def all_unique_paths(hash)
+      edge_paths(hash).flat_map do |edge|
+        list = ['/'] + edge.split('/').reject { |p| p == '' }
+        (0..list.size - 1).map { |i| list[0..i].join('/').gsub('//', '/') }
+      end.uniq
+    end
+
+    # adapted from: https://stackoverflow.com/questions/59495005/build-paths-of-edge-keys-in-nested-hashes-in-ruby
+    def edge_paths(hash)
+      hash.flat_map do |key, value|
+        if value.is_a?(Integer)
+          ''
+        elsif !value.is_a?(Hash) || value.empty?
+          key.to_s
+        else
+          edge_paths(value).map { |path| "#{key}/#{path}" }
+        end
+      end
     end
   end
 end
--- spec/calculators/directory_spec.rb
+++ spec/calculators/directory_spec.rb
@@ -38,6 +38,45 @@ describe Calculators::Directory do
     end
   end
 
+  describe '#run' do
+    before { subject.run }
+
+    context '#paths' do
+      describe 'root' do
+        let(:file_data) { { '/' => {} } }
+        let(:expected_result) { ['/'] }
+
+        it { expect(subject.paths).to match_array(expected_result) }
+      end
+
+      describe 'nested 1 level' do
+        let(:file_data) { { '/' => { 'a' => {} } } }
+        let(:expected_result) { ['/', '/a'] }
+
+        it { expect(subject.paths).to match_array(expected_result) }
+      end
+
+      describe 'nested 2 level' do
+        let(:file_data) do
+          { '/' =>
+            { 'a' => { 'b' => {} },
+              'c' => { 'd' => {} } } }
+        end
+        let(:expected_result) { ['/', '/a', '/c', '/a/b', '/c/d'] }
+
+        it { expect(subject.paths).to match_array(expected_result) }
+      end
+
+      describe 'full data set' do
+        let(:file_data) { file_data_all_levels}
+        let(:expected_result) do
+          ['/', '/a', '/d', '/a/e']
+        end
+
+        it { expect(subject.paths).to match_array(expected_result) }
+      end
+    end
+  end
 end
 # rubocop:enable Style/NumericLiterals
 # rubocop:enable Metrics/BlockLength
